866b372ddf6ec4ff7442f9d1371f9ded5ad5605d
Rushil Mittal <rushilmittal23@gmail.com>
2020-01-10 20:13:16 +0530

    sample code for spring secirity

---
+++ src/main/java/com/rushil/springsecurityjwt/models/AuthenticationResponse.java
@@ -0,0 +1,17 @@
+package com.rushil.springsecurityjwt.models;
+
+public class AuthenticationResponse 
+{ 
+	private final String jwt;
+
+	public AuthenticationResponse(String jwt) {
+		super();
+		this.jwt = jwt;
+	}
+
+	public String getJwt() {
+		return jwt;
+	}
+	
+	
+}
